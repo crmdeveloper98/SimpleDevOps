f9f157ccb2a5ba5d26d6473a267ae1c929ce449a
crmdeveloper98 <crmdeveloper98@gmail.com>
2021-05-01 06:46:42 +0100

    Added data annotations

--- SimpleDevOps/Models/PostViewModel.cs
+++ SimpleDevOps/Models/PostViewModel.cs
@@ -1,10 +1,20 @@
-﻿namespace SimpleDevOps.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SimpleDevOps.Models
 {
     public class PostViewModel
     {
+        [Required]
         public int Id { get; set; }
+
+        [Required]
         public string Title { get; set; }
+
+        [Required]
+        [MaxLength(4000)]
         public string Description { get; set; }
+
+        [Required]
         public string Author { get; set; }
     }
 }
